da625369b672e8b44f3860ba4c3aa8a3c72f5263
Juan Ricaldi <y2juanpablo@hotmail.com>
2019-07-12 18:50:23 -0400

    Tarea - Agregar login - password - enter fields

---
+++ src/main/java/testingui/diplomadoumss/org/basepackage/LoginTest.java
@@ -0,0 +1,26 @@
+/**
+ * @author: jricaldi
+ */
+
+package testingui.diplomadoumss.org.basepackage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import testingui.diplomadoumss.org.utilsfiles.PropertyAccesor;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BasePage {
+
+    private WebDriver webDriver;
+
+    @BeforeClass
+    public void init() {
+        webDriver = getWebDriver();
+        webDriver.get(PropertyAccesor.getInstance().getURL());
+    }
+
+}
